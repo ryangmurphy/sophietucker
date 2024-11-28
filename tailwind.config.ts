ff15c0857444752c3df78698fc4b679331a5a1df
ryangmurphy <ryangregorymurphy@outlook.com>
2024-11-28 17:17:21 -0400

    fixed tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,16 +78,11 @@ const config: Config = {
           "0%": { transform: "scale(0)" },
           "100%": { transform: "scale(1)" },
         },
-        "fade-in-up": {
-          "0%": { opacity: 0, transform: "translateY(20px)" },
-          "100%": { opacity: 1, transform: "translateY(0)" },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "scale-in": "scale-in 0.4s ease-out",
-        "fade-in-up": "fade-in-up 0.5s ease-out",
       },
     },
   },
